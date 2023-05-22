9064939149a0d61566ef73fd93dae58291365278
Emily Jin <emily.jin@cs.ox.ac.uk>
2023-05-23 00:26:44 +0100

    main

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Nodepiece
+#SBATCH --gres=gpu:1
+#SBATCH --time=12:00:00 
+#SBATCH --partition=short
+
+
+module load Anaconda3
+module load CUDA/10.1.243-GCC-8.3.0
+eval "$(conda shell.bash hook)"
+source activate $DATA/nodepiece-env
+
+which python
+
+python -u run_ilp.py $@ 
+
